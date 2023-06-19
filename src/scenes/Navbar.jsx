8e839e0202bd4d68c695868efd6367cef1261d7f
aenonsantiago <aejorisantiago@gmail.com>
2023-06-20 02:34:20 +0800

    Dotgroup - functionality

--- src/scenes/Navbar.jsx
+++ src/scenes/Navbar.jsx
@@ -3,7 +3,7 @@ import AnchorLink from "react-anchor-link-smooth-scroll";
 import useMediaQuery from "../hooks/useMediaQuery";
 import logo from "../assets/logo.png";
 import glitch from "../assets/glitch.gif";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 const Link = ({ page, selectedPage, setselectedPage }) => {
@@ -21,12 +21,13 @@ const Link = ({ page, selectedPage, setselectedPage }) => {
   );
 };
 
-const Navbar = ({ selectedPage, setselectedPage }) => {
+const Navbar = ({ isTopOfPage, selectedPage, setselectedPage }) => {
   const [isMenuToggled, setIsMenuToggled] = useState(false);
   const isAboveSmallScreens = useMediaQuery("(min-width: 768px)");
+  const navbarBackground = isTopOfPage ? "" : "bg-red";
 
   return (
-    <nav className="z-40 w-full fixed top-0 py-6">
+    <nav className={`${navbarBackground} z-40 w-full fixed top-0 py-6`}>
       <div className="flex items-center justify-between mx-auto w-5/6">
         <img
           className="absolute z-10 h-16 w-16 hover:opacity-0 bg-deep-red"
@@ -61,22 +62,46 @@ const Navbar = ({ selectedPage, setselectedPage }) => {
           </div>
         ) : (
           <button
-            className="rounded-full bg-red p-2" onclick={() => setIsMenuToggled(!isMenuToggled)}>            
-        <FontAwesomeIcon icon={faBars} />         
-        </button>
+            className="rounded-full bg-red p-2"
+            onclick={() => setIsMenuToggled(!isMenuToggled)}
+          >
+            <FontAwesomeIcon icon={faBars} />
+          </button>
         )}
 
         {/*MOBILE MENU POPUP*/}
         {!isAboveSmallScreens && isMenuToggled && (
-            <div className="fixed right-0 bottom-0 h-full bg-cyan-500 w-[300px]">
-                {/* CLOSE ICON */}
-                <div className="flex justify-end p-12">
-                    <button onClick={() => setIsMenuToggled(!isMenuToggled)}>
-                    <FontAwesomeIcon icon={faBars} />         
-                    </button>
+          <div className="fixed right-0 bottom-0 h-full bg-cyan-500 w-[300px]">
+            {/* CLOSE ICON */}
+            <div className="flex justify-end p-12">
+              <button onClick={() => setIsMenuToggled(!isMenuToggled)}>
+                <FontAwesomeIcon icon={faBars} />
+              </button>
             </div>
             {/* Menu Items */}
+            <div className="flex flex-col gap-10 ml-[33%] text-deep-red">
+              <Link
+                page="Home"
+                selectedPage={selectedPage}
+                setselectedPage={setselectedPage}
+              />
+              <Link
+                page="Skills"
+                selectedPage={selectedPage}
+                setselectedPage={setselectedPage}
+              />
+              <Link
+                page="Projects"
+                selectedPage={selectedPage}
+                setselectedPage={setselectedPage}
+              />
+              <Link
+                page="Contacts"
+                selectedPage={selectedPage}
+                setselectedPage={setselectedPage}
+              />
             </div>
+          </div>
         )}
       </div>
     </nav>

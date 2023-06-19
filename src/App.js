aeeba303c4b5ac4ea643838122ad4a96d060eb67
aenonsantiago <aejorisantiago@gmail.com>
2023-06-19 23:48:11 +0800

    AppJS - isTopOfPage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import useMediaQuery from "./hooks/useMediaQuery";
 import Navbar from "./scenes/Navbar";
 
 function App() {
 
   const [selectedPage, setSelectedPage] = useState('home');
+  cibst [isTopOfPage, setIsTopOfPage] = useState(true);
   const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) setIsTopOfPage(true);
+      if (window.scrollY !== 0) setIsTopOfPage(false); 
+    }
+  })
 
   return (
     <div className="app bg-deep-red">

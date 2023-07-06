82f90f1057dbf386a756716899815f81fbae8820
aenonsantiago <aejorisantiago@gmail.com>
2023-07-07 02:00:52 +0800

    MySkills Scene

---
+++ src/scenes/MySkills.jsx
@@ -0,0 +1,44 @@
+import LineGradient from "../components/LineGradient";
+import useMediaQuery from "../hooks/useMediaQuery";
+import { motion } from "framer-motion";
+
+const MySkills = () => {
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
+
+  return (
+    <section id="skills" className="pt-10 pb-24">
+      {/* Header and image section */}
+      <div className="md:flex md:justify-between md:gap-16 mt-32">
+        <motion.div
+          className="md:w-1/3"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+          <p className="font-playfair font-semibold text-4xl mb-5">
+            MY <span className="text-red">SKILLS</span>
+          </p>
+          <LineGradient width="w-1/3" />
+          <p className="mt-10 mb-7">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa ex
+            nulla corrupti illum optio odio aperiam doloribus amet explicabo,
+            laborum eaque molestias deserunt facilis assumenda eligendi harum
+            totam voluptates. Vel!
+          </p>
+        </motion.div>
+
+        <div className="mt-16 md:mt-0">
+            {isAboveMediumScreens ? (
+                <div 
+                
+            )}
+        </div>
+      </div>
+    </section>
+  );
+};

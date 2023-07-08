9b4e8c50d2ef448ab86b7fbf38cabb2dd5d4cb93
aenonsantiago <aejorisantiago@gmail.com>
2023-07-08 23:38:07 +0800

    Skills pt2

--- src/scenes/MySkills.jsx
+++ src/scenes/MySkills.jsx
@@ -34,8 +34,18 @@ const MySkills = () => {
 
         <div className="mt-16 md:mt-0">
             {isAboveMediumScreens ? (
-                <div 
-                
+              <div
+            className="relative z-0 ml-20 before:absolute before:-top-20 before:-left-20 
+                        before:rounded-t-[400px] before:w-full before:max-w-[400px] before:h-full
+                        before:border-2 before:border-blue before:z-[-1]"
+          >
+            <img
+              alt="profile"
+              className="hover:filter hover:saturate-200 transition duration-500 z-10 w-full
+                     max-w-[400px] md:max-w-[600px]"
+              src="../assets/zooo.png"
+            />
+          </div>
             )}
         </div>
       </div>
